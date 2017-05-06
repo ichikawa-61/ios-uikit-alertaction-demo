da03e21cfdc2a0f4e88997f0b943badb3e6b4011
Manami Ichikawa <mana@Ms-MacBook-Pro.local>
2017-05-06 11:42:39 +0900

    Add project

--- ios-uikit-alertaction-demo/ViewController.swift
+++ ios-uikit-alertaction-demo/ViewController.swift
@@ -12,14 +12,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    @IBAction func showAction(_ sender: Any) {
+        
+        //① UIAlertController のインスタンス生成
+        let alert: UIAlertController = UIAlertController(title: "アラート表示", message: "Alert テスト", preferredStyle:  UIAlertControllerStyle.alert)
+        
+        // ② Actionの設定
+        // Action初期化時にタイトル, スタイル, ボタンが押された時に実行される処理
+     
+        // OKボタン
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK",
+                                                         style: UIAlertActionStyle.default,
+                                                       handler:{
+                                                       (action: UIAlertAction!) -> Void in
+            print("OK")
+        })
+        // キャンセルボタン
+        let cancelAction: UIAlertAction = UIAlertAction(title: "キャンセル",
+                                                        style: UIAlertActionStyle.cancel,
+                                                      handler:{
+                                                      (action: UIAlertAction!) -> Void in
+            print("Cancel")
+        })
+        //　赤字
+        let destructiveAction: UIAlertAction = UIAlertAction(title: "消去",
+                                                             style: UIAlertActionStyle.destructive,
+                                                             handler:{
+                                                                (action: UIAlertAction!) -> Void in
+            print("delete")
+        
+        })
+        
+        // ③ UIAlertControllerにActionを追加
+        alert.addAction(cancelAction)
+        alert.addAction(defaultAction)
+        alert.addAction(destructiveAction)
+        
+        // ④ Alertを表示
+        present(alert, animated: true, completion: nil)
 
+    }
+    
+    
 
 }
 
